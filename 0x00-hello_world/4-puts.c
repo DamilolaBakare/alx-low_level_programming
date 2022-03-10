dc0e020aa163ff41cc9ae61583b3e91030d15156
Damilola Bakare <adamilola414@gmail.com>
2022-03-10 12:47:52 -0800

    programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *  main - Entry point
- *  Description: prints "\"Programming is like building a multilingual puzzle"
- *  Return: 0
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
  */
 
 int main(void)
