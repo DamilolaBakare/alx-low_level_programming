da997a3e4017302a251bda57dc238c4a63873d06
Damilola Bakare <adamilola414@gmail.com>
2022-03-15 05:44:43 -0700

    prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int i;
+
+  putchar ('X');
+  putchar (' ');
+  for (i=1; i<=10; i++)
+    {
+      putchar ('!');
+    }
+  putchar ('\n');
+
+  return 0;
+}
